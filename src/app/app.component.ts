3c8309e08fb0b789810e326ffa405ac6fd8cbd2c
gnopor <mayoublaise@gmail.com>
2021-06-01 21:21:32 +0100

    create header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'weather-chrono';
+  image_cold = 'assets/images/test.png';
+  bg_image = this.image_cold;
+
+  constructor() {}
+
+  getBgURL(image_name: string): string {
+    return `url(${image_name})`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ChronoComponent } from './components/chrono/chrono.component';
+import { WeatherComponent } from './components/weather/weather.component';
+import { HeaderComponent } from './components/header/header.component';
+
+const appRoutes: Routes = [
+  { path: '', component: WeatherComponent },
+  { path: 'chrono', component: ChronoComponent },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ChronoComponent, WeatherComponent, HeaderComponent],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
